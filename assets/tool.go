760c5701ea1d2916a7ba5b99349c946326140182
小宇宇 <3061196825@qq.com>
2026-03-28 21:51:11 +0800

    fix

--- assets/tool.go
+++ assets/tool.go
@@ -3,6 +3,7 @@ package assets
 import (
 	"fmt"
 	"os"
+	"sort"
 )
 
 func LoadCharacterActionFrames(roleId string, types string) (frames []string) {
@@ -11,5 +12,6 @@ func LoadCharacterActionFrames(roleId string, types string) (frames []string) {
 	for i := 0; i < len(entries); i++ {
 		frames = append(frames, uri+entries[i].Name())
 	}
+	sort.Strings(frames)
 	return
 }
